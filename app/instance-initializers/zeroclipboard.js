017239632845ee03de9fbe61420914fe98f8e42b
Tristan Edwards <tristan.edwards@me.com>
2016-02-27 13:11:41 +0100

    Update for Ember 2.x
    This fixes the error `lookupFactory is not a function` in newer Ember versions, while maintaining backwards-compatibility.

--- app/instance-initializers/zeroclipboard.js
+++ app/instance-initializers/zeroclipboard.js
@@ -1,10 +1,18 @@
 /* global ZeroClipboard */
 
+function lookupFactory(app, name) {
+  if (app.resolveRegistration) {
+   return app.resolveRegistration(name);
+ }
+
+ return app.container.lookupFactory(name);
+}
+
 export default {
   name: 'zeroclipboard',
 
   initialize: function(application) {
-    var config = application.container.lookupFactory('config:environment')['ember-zeroclipboard'];
+    var config = lookupFactory(application, 'config:environment')['ember-zeroclipboard'];
     if (config) { ZeroClipboard.config(config); }
   }
 };
