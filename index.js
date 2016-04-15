010a7f10247fd39758883644becd28879a4bbabb
Tristan Edwards <tristan.edwards@me.com>
2016-04-15 11:17:23 +0200

    Don't include JS library in Fastboot

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/zeroclipboard/dist/ZeroClipboard.js');
-
-    app.import(app.bowerDirectory + '/zeroclipboard/dist/ZeroClipboard.swf', {
-      destDir: 'assets'
-    });
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      app.import(app.bowerDirectory + '/zeroclipboard/dist/ZeroClipboard.js');
+  
+      app.import(app.bowerDirectory + '/zeroclipboard/dist/ZeroClipboard.swf', {
+        destDir: 'assets'
+      });
+    }
   }
 };
